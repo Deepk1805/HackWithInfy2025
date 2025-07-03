706b069f4e86a1140230e4c8d8ea908cbb62fd5d
Deepak Gautam <deepakgautam1089@gmial.com>
2025-07-03 09:52:01 +0530

    Buy_Two_Chocolates

--- GreedyApproach/Problem3_Buy_Two_Choclates.java
+++ GreedyApproach/Problem3_Buy_Two_Choclates.java
@@ -8,9 +8,19 @@ public class Problem3_Buy_Two_Choclates {
             prices[i]=s.nextInt();
         }
         int money=s.nextInt();
-        Arrays.sort(prices);
-        if(prices[0]+prices[1]<=money){
-            System.out.println( Math.abs(prices[0]+prices[1]-money));
+       int min1=Integer.MAX_VALUE;
+        int min2=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(min1>prices[i]){
+                min2=min1;
+                min1=prices[i];
+            }else if(prices[i]<min2){
+                min2=prices[i];
+            }
+        }
+        int cost=min1+min2;
+        if(cost<=money){
+            System.out.println( Math.abs(cost-money));
         }else{
             System.out.println( money);
         }
