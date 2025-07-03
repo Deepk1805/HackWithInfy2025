14c11fa3783cacbadeee3a641a73b29d91c4f0b1
Deepak Gautam <deepakgautam1089@gmial.com>
2025-07-03 10:26:06 +0530

    Buy_Two_Chocolates

--- GreedyApproach/Problem3_Buy_Two_Choclates.java
+++ GreedyApproach/Problem3_Buy_Two_Choclates.java
@@ -28,32 +28,3 @@ public class Problem3_Buy_Two_Choclates {
         s.close();
     }
 }
-
-
-
-
-
-
-            // OR 
-
-
-
-// import java.util.*;
-// public class Problem3_Buy_Two_Choclates {
-//     public static void main(String[] args) {
-//         Scanner s=new Scanner(System.in);
-//         int n=s.nextInt();
-//         int prices[]=new int[n];
-//         for(int i=0;i<n;i++){
-//             prices[i]=s.nextInt();
-//         }
-//         int money=s.nextInt();
-//        Arrays.sort(prices);
-//         if(prices[0]+prices[1]<=money){
-//             System.out.println( Math.abs(prices[0]+prices[1]-money));
-//         }else{
-//             System.out.println(money);
-//         }
-//         s.close();
-//     }
-// }
