6289c1823d9a3fceae55b820ea77c71d6d11a12d
Deepak Gautam <deepakgautam1089@gmial.com>
2025-07-07 13:12:28 +0530

    Problem7

--- GreedyApproach/problem7_FibonacciSeries.java
+++ GreedyApproach/problem7_FibonacciSeries.java
@@ -1,19 +1,52 @@
-import java.util.*;
-public class problem7_FibonacciSeries {
-    public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int n=s.nextInt();
-        int a=0;
-        int b=1;
-        int c=0;
-        for(int i=2;i<n;i++){
-            c=a+b;
-            a=b;
-            b=c;
+import java.util.Scanner;
+
+class Solution {
+    public int nthFibonacci(int n) {
+        if (n <= 1) {
+            return n;
         }
-        System.out.println(c);
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Solution sol = new Solution();
+        System.out.println(sol.nthFibonacci(n));
+        sc.close();
     }
 }
+
+
+
+// OR
+
+
+// import java.util.*;
+// public class problem7_FibonacciSeries {
+//     public static void main(String[] args) {
+//         Scanner s=new Scanner(System.in);
+//         int n=s.nextInt();
+//         int a=0;
+//         int b=1;
+//         int c=0;
+//         for(int i=2;i<n;i++){
+//             c=a+b;
+//             a=b;
+//             b=c;
+//         }
+//         System.out.println(c);
+//     }
+// }
 
 
 // OR
