f4313440041d8570b9eacb1019004b064be65f48
Deepak Gautam <deepakgautam1089@gmial.com>
2025-07-08 13:16:15 +0530

    New

--- DynamicProgramming/Problem11_01Knapsack.java
+++ DynamicProgramming/Problem11_01Knapsack.java
@@ -1,3 +1,33 @@
-public class Problem11_01Knapsack {
-    
+import java.util.Scanner;
+class Problem11_01Knapsack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] weights = new int[n];
+        int[] values = new int[n];
+        for (int i = 0; i < n; i++) {
+            weights[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            values[i] = sc.nextInt();
+        }
+        int capacity = sc.nextInt();
+        System.out.println(knapSack(weights, values, capacity));
+    }
+    public static int knapSack(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= capacity; j++) {
+                if (weights[i - 1] <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + values[i - 1]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][capacity];
+    }
 }
